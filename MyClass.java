f093cd32992821560c04ad4d050e72d90ec9a941
Fred Glaeser IV <fredjg4@gmail.com>
2019-08-20 09:58:03 -0400

    Added a comment

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,10 @@
 public class MyClass{
 
     public static void main(String[] args){
+
+        // myComment
         System.out.println("Hello World");
+
         if(args.length>0){
             System.out.println(args[0]);
         }
