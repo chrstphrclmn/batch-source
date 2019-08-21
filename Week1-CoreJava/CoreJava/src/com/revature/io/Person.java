f075ac97ddfc14a9833c21b8c0e9d175d67cad5a
carolynrehm <carolynhrehm@gmail.com>
2019-08-21 14:42:07 -0400

    serialization example

---
+++ Week1-CoreJava/CoreJava/src/com/revature/io/Person.java
@@ -0,0 +1,63 @@
+package com.revature.io;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+	
+	private static final long serialVersionUID = -8516458329335962601L;
+	
+	private transient int id;
+	private String name;
+	private String email;
+	
+	public Person() {
+		super();
+	}
+
+	public Person(int id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+
+}
