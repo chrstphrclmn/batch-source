bd625da48fcf39b159d3727e22eef4fd111d7ba0
KaylanHusband <kaylanhusband@gmail.com>
2019-08-21 12:44:30 -0400

    Assignment 1 commit 2

--- Assignment1/src/com/revature/assignment1/Car.java
+++ Assignment1/src/com/revature/assignment1/Car.java
@@ -3,6 +3,30 @@ package com.revature.assignment1;
 import java.util.Scanner;
 
 public abstract class Car implements Drivable {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((licensePlate == null) ? 0 : licensePlate.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		if (licensePlate == null) {
+			if (other.licensePlate != null)
+				return false;
+		} else if (!licensePlate.equals(other.licensePlate))
+			return false;
+		return true;
+	}
+
 	private Scanner sc = new Scanner(System.in);
 	
 	private String licensePlate;
