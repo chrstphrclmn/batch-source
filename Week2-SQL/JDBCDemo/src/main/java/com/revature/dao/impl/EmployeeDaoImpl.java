feb5ca1d42b520fafabfc30b5b3b28c5eb678fca
carolynrehm <carolynhrehm@gmail.com>
2019-08-29 11:59:22 -0400

    thursday morning jdbc

---
+++ Week2-SQL/JDBCDemo/src/main/java/com/revature/dao/impl/EmployeeDaoImpl.java
@@ -0,0 +1,40 @@
+package com.revature.dao.impl;
+
+import java.util.List;
+
+import com.revature.dao.EmployeeDao;
+import com.revature.model.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+
+	@Override
+	public List<Employee> getEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee getEmployeeById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int createEmployee(Employee e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateEmployee(Employee e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteEmployeeById(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- Week2-SQL/JDBCDemo/src/main/java/com/revature/util/ConnectionUtil.java
+++ Week2-SQL/JDBCDemo/src/main/java/com/revature/util/ConnectionUtil.java
@@ -1,50 +1,67 @@
 package com.revature.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
-	
+
 	private static Connection connection;
-	
+
 	public static Connection getHardCodedConnection() throws SQLException {
-		
+
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		String url = "jdbc:postgresql://[url]:5432/postgres";
 		String username = "user";
 		String password = "pass";
-		
+
 		if(connection == null || connection.isClosed()) {
 			connection = DriverManager.getConnection(url, username, password);
 		}
-		
+
 		return connection;
 	}
-	
-public static Connection getConnection() throws SQLException {
-		
+
+	public static Connection getConnectionFromFile() throws IOException, SQLException {
+		Properties prop = new Properties();
+		InputStream in = new FileInputStream("connection.properties");
+		prop.load(in);
+		String url = prop.getProperty("url");
+		String username = prop.getProperty("username");
+		String password = prop.getProperty("password");
+		if(connection == null || connection.isClosed()) {
+			connection = DriverManager.getConnection(url, username, password);
+		}
+		return connection;
+	}
+
+	public static Connection getConnection() throws SQLException {
+
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		String url = System.getenv("DB_URL");
 		String username = System.getenv("DB_USER");
 		String password = System.getenv("DB_PASS");
-		
+
 		if(connection == null || connection.isClosed()) {
 			connection = DriverManager.getConnection(url, username, password);
 		}
-		
+
 		return connection;
 	}
-	
+
 
 }
