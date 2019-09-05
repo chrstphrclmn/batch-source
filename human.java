e015d92b99e328b00e07d76ac86b9b3933c1edf3
Abdellatif Elmansouri <axelmansouri1@gmail.com>
2019-09-05 14:14:37 -0400

    html added

--- human.java
+++
@@ -1,9 +0,0 @@
-public class human {
-
-    //private static final String HELLO_WORD = "helloWord";
-
-    public static void main(String[] args) {
-        System.out.println("HELLO_WORD");// +abdo1.toString());
-	
-	}
-}
