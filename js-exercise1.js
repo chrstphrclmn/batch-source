95b5591ec715d8b8b37956d187ef3723ef4884c7
Shirosaya25 <Michaelzh2009@gmail.com>
2019-09-05 15:06:41 -0400

    Challenge Problems Completed

--- js-exercise1.js
+++ js-exercise1.js
@@ -124,3 +124,207 @@ function isPalindrome(str){
 
     return x.toLowerCase() === str.toLowerCase();
 }
+
+// Q9: Leap Year
+
+function isLeapYear(date){
+
+    let year = date.getFullYear();
+
+    if(year % 4) return false;
+    else if(year % 100) return true;
+    else if(year % 400) return false;
+    else return true;
+}
+
+// Q10: Shapes
+
+function printShape(shape, height, char){
+
+    let line = "";
+
+    if(shape.toLowerCase() == "square"){
+
+        for(let i = 1 ; i <= height ; i++){
+
+            line = "";
+
+            for(let j = 1 ; j <= height ; j ++){
+
+                line += char;
+            }
+
+            console.log(line);
+        }
+    }
+
+    if(shape.toLowerCase() == "triangle"){
+
+        for(let i = 1 ; i <= height ; i++){
+
+            line = "";
+
+            for(let j = 1 ; j <= i ; j ++){
+
+                line += char;
+            }
+
+            console.log(line);
+        }
+    }
+
+    if(shape.toLowerCase() == "diamond"){
+
+        if(height % 2){
+
+            for(let i = 1 ; i <= height ; i += 2){
+
+                line = "";
+
+                for(let j = 1 ; j <= (height - i) / 2 ; j++){
+
+                    line += " ";
+                }
+
+                for(let j = 1 ; j <= i ; j ++){
+
+                    line += char;
+                }
+
+                for(let j = 1 ; j <= (height - i) / 2 ; j++){
+
+                    line += " ";
+                }
+
+            console.log(line);
+
+            }
+
+            for(let i = height - 2; i >= 1 ; i -= 2){
+
+                line = "";
+
+                for(let j = 1 ; j <= (height - i) / 2 ; j++){
+
+                    line += " ";
+                }
+
+                for(let j = 1 ; j <= i ; j ++){
+
+                    line += char;
+                }
+
+                for(let j = 1 ; j <= (height - i) / 2 ; j++){
+
+                    line += " ";
+                }
+
+            console.log(line);
+
+            }
+        }
+
+        else{
+
+            for(let i = 2 ; i <= height ; i += 2){
+
+                line = "";
+
+                for(let j = 1 ; j <= (height - i) / 2 ; j++){
+
+                    line += " ";
+                }
+
+                for(let j = 1 ; j <= i ; j ++){
+
+                    line += char;
+                }
+
+                for(let j = 1 ; j <= (height - i) / 2 ; j++){
+
+                    line += " ";
+                }
+
+            console.log(line);
+
+            }
+
+            for(let i = height - 2; i >= 2 ; i -= 2){
+
+                line = "";
+
+                for(let j = 1 ; j <= (height - i) / 2 ; j++){
+
+                    line += " ";
+                }
+
+                for(let j = 1 ; j <= i ; j ++){
+
+                    line += char;
+                }
+
+                for(let j = 1 ; j <= (height - i) / 2 ; j++){
+
+                    line += " ";
+                }
+
+            console.log(line);
+
+            }
+        }
+    }
+}
+
+// Q11 : Rotate left
+
+function rotate(arr, n){
+
+    let offset = n % arr.length;
+    let temp;
+    
+    for(let i = 0 ; i < offset ; i++){
+
+        temp = arr[0];
+
+        for(let j = 0 ; j < arr.length - 1 ; j++){
+
+            arr[j] = arr[j + 1];
+        }
+
+        arr[arr.length - 1] = temp;
+    }
+
+    return arr;
+}
+
+// Q12: Bracket Balance
+
+function balance(str){
+
+    let stack = [];
+
+    for(let char of str){
+
+        if(char == '(') stack.push(')');
+        else if (char == '[') stack.push(']');
+        else if (char == '{') stack.push('}');
+
+        else if (char == ')'){
+
+            if(char != stack.pop()) return false;
+        }
+
+        else if (char == ']'){
+
+            if(char != stack.pop()) return false;
+        }
+
+        else if (char == '}'){
+
+            if(char != stack.pop()) return false;
+        }
+    }
+
+    return(!stack.length);
+
+}
