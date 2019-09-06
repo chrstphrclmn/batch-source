8e369dd042eda0476e036e9957907c3e23fa243b
Samuel Dorilas <samueldorilas@outlook.com>
2019-09-05 22:08:03 -0400

    Added challenges

--- js_exercise/js/js-exercise1.js
+++ js_exercise/js/js-exercise1.js
@@ -40,12 +40,29 @@ window.onload = function() {
   console.log(isLeapYear(date3));
   console.log(isLeapYear(date4));
 
-  
-  printShape("Square", 3, "$");
+  console.log("Print Shape");
+  printShape("Square", 3, "%");
   printShape("Triangle", 3, "$");
-  printShape("Diamond", 3, "$");
+  printShape("Diamond", 3, "*");
+  printShape("Diamond", 5, "*");
+
+  console.log("Rotate");
+  console.log(rotate([1, 2, 3, 4, 5], 1));
+  console.log(rotate([1, 2, 3, 4, 5], 6));
+  console.log(rotate([1, 2, 3, 4, 5], 3));
+
+  console.log("Balanced");
+  console.log(balanced("()"));
+  console.log(balanced("()()"));
+  console.log(balanced("(())"));
+  console.log(balanced("({[]})"));
+  console.log(balanced("("));
+  console.log(balanced(")"));
+  console.log(balanced("(()i"));
+  console.log(balanced("([)]"));
 };
 
+//O(n)
 function maxLength(array) {
   let length = 0;
   let index = -1;
@@ -57,10 +74,12 @@ function maxLength(array) {
   return index;
 }
 
+//O(n)
 function reverseArray(array) {
   return array.reverse();
 }
 
+//O(5n) = O(n)
 function vowelCount(string) {
   let count = 0;
   let vowels = ["a", "e", "i", "o", "u"];
@@ -75,15 +94,18 @@ function vowelCount(string) {
   return count;
 }
 
+//O(1)
 function isValidEmail(email) {
   let re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
 
+//O(n)
 function removeChar(string, index) {
   return string.replace(string[index], "");
 }
 
+//O(n^2) worst case not close to being sorted
 function bubbleSort(numArray) {
   let length = numArray.length - 1;
   let swapped = true;
@@ -101,6 +123,7 @@ function bubbleSort(numArray) {
   return numArray;
 }
 
+//O(1)
 function isEven(someNum) {
   if (Number.isInteger(someNum / 2)) {
     return true;
@@ -109,6 +132,7 @@ function isEven(someNum) {
   }
 }
 
+//O(n)
 function isPalindrome(someStr) {
   let newString = someStr
     .split("")
@@ -116,7 +140,9 @@ function isPalindrome(someStr) {
     .join("");
   return newString.toUpperCase() == someStr.toUpperCase();
 }
+
 // Leap Years are any year that can be exactly divided by 4 (such as 2016, 2020, 2024, etc). not, except if it can be exactly divided by 100,
+//O(1)
 function isLeapYear(date) {
   let year = date.getYear();
   if (year % 4 == 0 && year % 100 != 0) {
@@ -127,27 +153,85 @@ function isLeapYear(date) {
 }
 
 //"Square", "Triangle", "Diamond".
+//O((n/2)^2) -> O(n^2/4) -> O(n^2)
 function printShape(shape, height, character) {
   switch (shape) {
     case "Square":
-        for(let i = 0; i < height; i++){console.log(character+character+character);}
+      for (let i = 0; i < height; i++) {
+        console.log(character + character + character);
+      }
       break;
     case "Triangle":
-
+      for (let i = 0; i < height; i++) {
+        let row = "";
+        for (let j = 0; j < i + 1; j++) {
+          row += character;
+        }
+        console.log(row);
+      }
       break;
     case "Diamond":
+      if (height % 2 == 0 || height <= 0) {
+        console.log("invalid diamond");
+      }
+      let half = Math.floor(height / 2);
+      for (let i = 0; i < half + 1; i++) {
+        let row = new Array(height - 1);
+        for (let j = 0; j < half + 1; j++) {
+          if (j >= half - i) {
+            row[j] = character;
+            row[height - 1 - j] = character;
+          } else {
+            row[j] = " ";
+            row[height - 1 - j] = " ";
+          }
+        }
+        console.log(row.join(""));
+      }
+      for (let i = half - 1; i >= 0; i--) {
+        let row = [];
+        for (let j = 0; j < half + 1; j++) {
+          if (j >= half - i) {
+            row[j] = character;
+            row[height - 1 - j] = character;
+          } else {
+            row[j] = " ";
+            row[height - 1 - j] = " ";
+          }
+        }
+        console.log(row.join(""));
+      }
       break;
   }
 }
 
-// function test(){
-//     name: "hi"
-// }
+//O(n)
+function rotate(array, n) {
+  let length = array.length;
+  let rotations = n > length ? n - length : n;
+  return [...array.slice(rotations), ...array.slice(0, rotations)];
+}
 
-// test2{
-//     hello: "me"
-// }
+//O(n^2)
+function balanced(string) {
+  let length = string.length;
+  for (let i = 0; i < length; i++) {
+    if (!(i = scan(i, string))) {
+      return false;
+    }
+  }
+  return true;
+}
 
-// test3{
-//     hello: "me"
-// }
+function scan(index, string) {
+  let opening = ["(", "{", "[",")", "}", "]"];
+  let closing = [")", "}", "]","(", "{", "["];
+  for (let i = index + 1; i < string.length; i++) {
+    let oIndex = opening.indexOf(string[index]);
+    if (string[i] != closing[oIndex]) {
+      i = scan(i, string);
+    } else {
+      return i;
+    }
+  }
+}
